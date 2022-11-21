d1a74431105d88cafe405c8b476eac622cace0e6
Kontawat Wisetpaitoon <70422920+Nightsky-na@users.noreply.github.com>
2022-11-22 06:01:57 +0700

    hero page and css

--- Elastic_Search_Update/app.py
+++ Elastic_Search_Update/app.py
@@ -6,6 +6,7 @@ import math
 import os
 from dotenv import load_dotenv
 import certifi
+import pandas as pd 
 load_dotenv()
 
 ELASTIC_PASSWORD = os.getenv('ELASTIC_PASSWORD')
@@ -19,8 +20,24 @@ app = Flask(__name__,template_folder='template')
 dict_image_folder = {
     'herodota' : 'images_Dota2',
     'herolol' : 'images_LoL',
-    'herorov' : 'images_LoL'
+    'herorov' : 'images_rov'
 }
+
+df_lol = pd.read_csv("./static/data/Data_LOL.csv")
+df_rov = pd.read_csv("./static/data/Data_ROV.csv")
+df_dota = pd.read_json("./static/data/herodota.json",lines=True)
+
+def get_data(game_type, hero):
+    if (game_type == "herodota"):
+        data_helo = df_dota.loc[df_dota['Name'] == hero]
+    elif (game_type == "herolol"):
+        data_helo = df_lol.loc[df_lol['Name'] == hero+" "]
+    elif (game_type == "herorov"):
+        data_helo = df_rov.loc[df_rov['Name'] == hero]
+    print(data_helo)
+    data_helo = data_helo.to_dict('r')
+    return data_helo
+
 
 @app.route('/')
 def index():
@@ -61,3 +78,12 @@ def search():
         return render_template('search.html', game_type=game_type, keyword=keyword, hits=hits, page_no=page_no, page_total=page_total, image_folder=dict_image_folder[game_type])
     else:
         return render_template('notFound.html', game_type=game_type, keyword=keyword, hits=hits, page_no=page_no, page_total=page_total, image_folder=dict_image_folder[game_type])
+
+@app.route('/heroDetail/<gametype>/<name>')
+def moredetails(gametype, name):
+    data = get_data(gametype, name)[0]
+    # print(data[0])
+    data['Type'] = eval(data['Type'])
+    data['Skills'] = eval(data['Skills'])
+    print(type(data['Skills']))
+    return render_template('hero.html',doc=data,image_folder=dict_image_folder[gametype],gametype=gametype)
